59aae246c4fffb38dd8bf84e459d9d566302cd3f
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-18 17:48:28 +0700

    add public bool listempty and public bool ascending

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -118,5 +118,27 @@ namespace double_linked_list
             current.next.prev = previous;
             return true;
         }
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void ascending()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is Empty");
+            else
+            {
+                Console.WriteLine("\nRecord in the ascending order of" + "Roll number are:\n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.noMhs + currentNode.next.name + "\n");
+            }
+        }
+        public void descending()
+        ()
     }
 }
