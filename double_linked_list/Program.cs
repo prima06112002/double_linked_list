59e314c072aa3b89a8f8424292c643ef480677fc
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-18 17:09:59 +0700

    add class node and class doublelinkedlist

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -8,12 +8,51 @@ namespace double_linked_list
 {
     class Node
     {
-
+        /*Node class reperesent the  node of doubly linked list
+         * it consist of the information part and and link to
+         * its secceding amd preceeding
+         * in terms of the next and previous */
+        public int noMhs;
+        public string name;
+        //point to succeding node
+        public Node next;
+        //point to preceeding node
+        public Node prev;
     }
-    internal class Program
+    class DoubleLinkedList
     {
-        static void Main(string[] args)
+        Node START;
+
+        //constructor
+
+
+        public void addnode()
         {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("/nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+                {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number of allowed");
+                    return;
+
+                }
+                /*On the execution of the above for loop,prev and
+                 * current will point to those nodes
+                 * between which the new node is to be inserted*/
+                newNode.next = current;
+                newNode.prev = previous;
+            }
         }
     }
 }
